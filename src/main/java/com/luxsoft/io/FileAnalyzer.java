3a97ceab5c6047e0919ff1b3157d5479d0fbfaf9
Igor <igor@MacBook-Pro-Igor.local>
2021-12-05 04:17:22 +0200

    commit FileAnalyzer

--- src/main/java/com/luxsoft/io/FileAnalyzer.java
+++ src/main/java/com/luxsoft/io/FileAnalyzer.java
@@ -1,20 +1,5 @@
 package com.luxsoft.io;
 
-
-/*Используем классы FileInputStream, FileOutputStream, File
-Практика:
-1: Написать программу FileAnalyzer, которая в консоли принимает 2 параметра:
-1) путь к файлу
-2) слово
-Usage:
-java FileAnalyzer C:/test/story.txt duck
-
-Выводит:
-1) Кол-во вхождений искомого слова в файле
-2) Все предложения содержащие искомое слово(предложение заканчивается символами ".", "?", "!").
-Каждое предложение выводится с новой строки.*/
-
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
